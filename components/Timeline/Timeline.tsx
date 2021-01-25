9515d596a9b5e9cd454fffb450c8065b8c20a8a5
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-01-25 19:37:40 +0530

    chore(timeline): update timeline

--- components/Timeline/Timeline.tsx
+++ components/Timeline/Timeline.tsx
@@ -4,30 +4,44 @@ import styles from "./Timeline.module.css";
 export default function Timeline() {
   let itemsList1 = [
     {
-      cardTitle: "1st February 2021",
-      cardSubtitle: "JavaScript Session",
+      cardTitle: "1 February 2021",
+      cardSubtitle: "Hello JavaScript!",
     },
     {
-      cardTitle: "3rd February 2021",
-      cardSubtitle: "Introducing Node.js",
+      cardTitle: "3 February 2021",
+      cardSubtitle: "What is Node.js?",
     },
     {
-      cardTitle: "5th February 2021",
-      cardSubtitle: "Advanced concepts of Node.js",
+      cardTitle: "5 February 2021",
+      cardSubtitle: "Deeper into Node.js",
     },
   ];
   let itemsList2 = [
     {
-      cardTitle: "7th February 2021",
-      cardSubtitle: "Intro to AWS",
+      cardTitle: "7 February 2021",
+      cardSubtitle: "Amazing AWS",
     },
     {
-      cardTitle: "9th February 2021",
-      cardSubtitle: "Advanced Concepts of AWS",
+      cardTitle: "9 February 2021",
+      cardSubtitle: "Advanced AWS",
     },
     {
-      cardTitle: "11th February 2021",
+      cardTitle: "11 February 2021",
       cardSubtitle: "Discovering Alexa Skills",
+    },
+  ];
+  let itemsList3 = [
+    {
+      cardTitle: "February 2021 (Second Half)",
+      cardSubtitle: "Project I",
+    },
+    {
+      cardTitle: "March 2021 (First Half)",
+      cardSubtitle: "Project II",
+    },
+    {
+      cardTitle: "March 2021 (Second Half)",
+      cardSubtitle: "Project III",
     },
   ];
   return (
@@ -41,7 +55,7 @@ export default function Timeline() {
         >
           Timeline
         </h1>
-        <div className="grid grid-cols-2 w-full space-x-5 pb-16">
+        <div className="grid grid-cols-3 w-full space-x-5 pb-16">
           <div className="col-span-3 md:col-span-1">
             <Chrono
               items={itemsList1}
@@ -73,7 +87,20 @@ export default function Timeline() {
               }}
             />
           </div>
-          
+          <div className="col-span-3 md:col-span-1" style={{ marginLeft: 0 }}>
+            <Chrono
+              items={itemsList3}
+              mode="VERTICAL"
+              disableNavOnKey
+              hideControls
+              theme={{
+                primary: "#FB905D",
+                secondary: "#FB905D",
+                cardBgColor: "#393E5B",
+                cardForeColor: "white",
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
