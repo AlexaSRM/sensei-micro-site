d5a62f63d54a5c8dee09ee1d55c98aefd6b382bc
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-01-25 17:36:15 +0530

    chore(img): vertically center images

--- components/Sponsor/Sponsor.tsx
+++ components/Sponsor/Sponsor.tsx
@@ -1,33 +1,32 @@
-import { Chrono } from 'react-chrono';
-import styles from './Sponsor.module.css';
+import { Chrono } from "react-chrono";
+import styles from "./Sponsor.module.css";
 export default function Sponsor() {
   let sponsors = [
     {
-      image: '/seaport.jpeg',
-      name: 'Seaport AI',
-      title: 'Title Sponsor'
+      image: "/seaport.jpeg",
+      name: "Seaport AI",
+      title: "Title Sponsor",
     },
     {
-      image: '/grabon.png',
-      name: 'GrabOn',
-      title: 'Official Savings Partner'
+      image: "/grabon.png",
+      name: "GrabOn",
+      title: "Official Savings Partner",
     },
     {
-      image: '/sashido.png',
-      name: 'Sashido',
-      title: 'Ideation Partner'
+      image: "/sashido.png",
+      name: "Sashido",
+      title: "Ideation Partner",
     },
     {
-      image: '/tech_crawl.png',
-      name: 'Tech Crawl',
-      title: 'Media Partner'
+      image: "/tech_crawl.png",
+      name: "Tech Crawl",
+      title: "Media Partner",
     },
     {
-      image: '/replit.svg',
-      name: 'Repl.it',
-      title: 'Sponsor'
+      image: "/replit.svg",
+      name: "Repl.it",
+      title: "Sponsor",
     },
-
   ];
   return (
     <div className="w-full">
@@ -39,22 +38,38 @@ export default function Sponsor() {
           ].join(" ")}
         >
           Sponsors
-        </h1> 
+        </h1>
         <div
-          className="grid grid-cols-1 gap-6 sm:grid-cols-5 gap-4"
+          className="grid grid-cols-1 gap-6 sm:grid-cols-5 gap-4 mb-20"
           style={{ marginLeft: 0 }}
-        >{
-           sponsors.map(listItem => (
-                  <div className="group block mx-auto flex flex-col" key={listItem.name}>
-                    <div className={styles.sponsorImage}>
-                    <img className="w-20" src={listItem.image} alt={listItem.name}/>
-                    </div>
-                    <div className={styles.sponsorText}><p className="mx-auto font-semibold text-4x1 text-center">{listItem.name}</p>
-                    <p className="mx-auto text-center">{listItem.title}</p></div>
-                  </div>
-            ))
-            }
-            <br/><br/>
+        >
+          {sponsors.map((listItem) => (
+            <div
+              className="group block mx-auto flex flex-col"
+              key={listItem.name}
+            >
+              <div className={[styles.sponsorImage, ""].join(" ")}>
+                <div className="relative h-72 w-48 mx-auto">
+                  <img
+                    className="absolute w-auto my-auto mx-auto top-0 bottom-0 right-0 left-0"
+                    src={listItem.image}
+                    alt={listItem.name}
+                  />
+                </div>
+
+                <div className={styles.sponsorText}>
+                  <p className="mx-auto font-semibold text-4xl text-center">
+                    {listItem.name}
+                  </p>
+                  <p className="mx-auto text-2xl text-center">
+                    {listItem.title}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+          <br />
+          <br />
         </div>
       </div>
     </div>
